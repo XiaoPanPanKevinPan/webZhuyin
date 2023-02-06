15c6b1b8da142b910a20ca30fa2f2eae3d5cde2f
user <user@Debian-KevinPan>
2023-02-06 16:34:54 +0800

    add 1qazToBopomofo.js - separate 1qazToBopomofo.js from example.html - add charset=unicode in example.html

---
+++ 1qazToBopomofo.js
@@ -0,0 +1,48 @@
+const createInputEvents = (options) => ({
+	beforeInput: new InputEvent("beforeinput", options),
+	input: new InputEvent("input", options)
+});
+
+export async function translator(e){
+	// `e` should be an keydown event
+
+	const orig = "1qaz2wsxedcrfv5tgbyhnujm8ik,9ol.0p;/-= 6347'",
+	      symb = "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ ˉˊˇˋ˙'";
+	const loc = orig.indexOf(e.key);
+	if(loc == -1 || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || e.code.match(/^Numpad.*/))
+		return;
+	e.preventDefault();
+
+	// translate key to zhuyin symbols
+	let newstr = "", t = e.target;
+
+	newstr += symb[loc];
+	if(" 6347'".indexOf(e.key) != -1) newstr += " ";
+
+	// create input events
+	const {beforeInput: eBeforeInput, input: eInput} =
+		await createInputEvents({
+			inputType: "insertText",
+			data: newstr,
+			isComposing: false
+		});
+
+	// dispatch beforeinput event
+	t.dispatchEvent(eBeforeInput);
+
+	// insert text
+	t.setRangeText(newstr, t.selectionStart, t.selectionEnd, "end");
+
+	// dispatch input event
+	t.dispatchEvent(eInput);
+
+	return;
+};
+
+export function createZhuyinTextarea(){
+	let elem = document.createElement("textarea");
+	elem.addEventListener("keydown", translator);
+
+	return elem;
+}
+
