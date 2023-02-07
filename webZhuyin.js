41b28a5e9fbe8b5e6b44881b836ca1cc3e477b20
user <user@Debian-KevinPan>
2023-02-07 21:54:42 +0800

    webZhuyin.js parseZhuyin() if the parsed tone is '', set it as '&nbsp;'

--- webZhuyin.js
+++ webZhuyin.js
@@ -32,7 +32,7 @@ export function parseZhuyin(zhuyin = "", {firstTone = "&nbsp;"} = {}) {
 	return zhuyin
 		.split(/[\ \n]+|(?<=[ˉˊˇˋ˙])/)
 		.map(zhuyin => zhuyin.split(/(?=[ˉˊˇˋ˙])/))
-		.map(([symb = "", tone = "　"]) => [
+		.map(([symb = "", tone = "&nbsp;"]) => [
 			symb == "'" ? "" : symb,
 			tone == 'ˉ' ? firstTone : tone
 		]);
